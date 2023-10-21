ba9fa8fe97e0944ad44e2c615d397faf3697ceb2
Gaaaybe <gabenesus@gmail.com>
2023-10-21 00:39:58 -0300

    Criação dos metodos insert(no inicio) e to array

--- atividades/src/atividade03/ListaEncadeada.java
+++ atividades/src/atividade03/ListaEncadeada.java
@@ -5,60 +5,74 @@ public class ListaEncadeada implements ListaEncadeada_IF {
     public Integer dado;
     public ListaEncadeada prox;
 
-    public boolean isEmpty(){
-        if (dado == null){
+    public boolean isEmpty() {
+        if (dado == null) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-
-	public Integer size(){
-        if(isEmpty()){
+    public Integer size() {
+        if (isEmpty()) {
             return 0;
-        }
-        else{
-            return 1+prox.size();
+        } else {
+            return 1 + prox.size();
 
         }
     }
-	public Integer search(Integer element) throws Exception{
-        if(isEmpty()){
+
+    public Integer search(Integer element) throws Exception {
+        if (isEmpty()) {
             throw new Exception("Valor não encontrado");
-        }
-        else{
-            if (dado == element){
+        } else {
+            if (dado == element) {
                 return dado;
-            }
-            else{
+            } else {
                 return prox.search(element);
             }
         }
 
     }
 
-    public void insert(Integer element){}
+    public void insert(Integer element) {
 
-
-	public void remove(Integer element){
-            if(isEmpty()){
-            
+        if (isEmpty()) {
+            dado = element;
         }
-        else{
-            if(dado == element){
+        ListaEncadeada novalista = new ListaEncadeada();
+        novalista.dado = element;
+        novalista.prox = this;
+        this.prox = novalista;
+
+    }
+
+    public void remove(Integer element) {
+        if (isEmpty()) {
+
+        } else {
+            if (dado == element) {
                 dado = prox.dado;
                 prox = prox.prox;
             }
         }
 
     }
-	public Integer[] toArray() {
- 
+
+    public Integer[] toArray() {
+        Integer[] resultados = new Integer[size()];
+        auxtoArray(resultados, 0, this);
+        return resultados;
 
     }
 
+    public void auxtoArray(Integer[] vetor, int index, ListaEncadeada no) {
+        if (!no.isEmpty()) {
+            vetor[index] = no.dado;
+            if (no.prox != null) {
+                auxtoArray(vetor, index + 1, no.prox);
+            }
+        }
+    }
 
-    
 }
